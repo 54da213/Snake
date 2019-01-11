9381872bfe4b522ab84ddd03484b16c9c4895f52
54da213 <936018838@qq.com>
2019-01-11 22:20:49 +0800

    重构完成解决画布内存泄漏

--- game.py
+++ game.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 import Tkinter
-import threading
 import random
 import enum
-import time
 
-
-def app():
-    con = Controller()
-    con.start_game()
-
-
-VIEW_WIDTH = 400
+BODY = 10
+SPEED = 500
+MAP_BG = "#030915"
+FOOD_BG = "green"
+SNAKE_BG = "#2EB7DE"
 VIEW_HIGH = 400
-BODY = 20
+VIEW_WIDTH = 400
 
 
 # 方向控制
@@ -24,109 +20,123 @@ class Direction(enum.Enum):
     right = "d"
 
 
-# 绘制地图
-class View(object):
-    def __init__(self):
-        self.top = Tkinter.Tk()
-        self.top.geometry("400x400")
-        self.cv = Tkinter.Canvas(self.top, bg="green", height=VIEW_HIGH, width=VIEW_WIDTH)
-
-    def draw(self):
-        # 绘地图
-        for i in xrange(0, 20):
-            for j in xrange(0, 20):
-                self.cv.create_rectangle(20 * i, 20 * j, 20 * i + 50, 20 * j + 20, fill="red")
-        self.cv.pack()
-
-    def food(self, points):
-        x1, y1, x2, y2 = points
-        self.cv.create_rectangle(x1, y1, x2, y2, fill="green")
-
-    def snake(self):
-        # 绘蛇
-        color = snake.body_color
-        snake_len = len(snake.bodys)
-        for i in xrange(0, snake_len):
-            if i == 2:
-                color = snake.head_color
-            x1, y1, x2, y2 = snake.bodys[i]
-            self.cv.create_rectangle(x1, y1, x2, y2, fill=color)
-        # self.top.mainloop()
-
-    def update(self, **kwargs):
-        # 新蛇头
-        x1, y1, x2, y2 = snake.bodys[-1]
-        self.cv.create_rectangle(x1, y1, x2, y2, fill=snake.head_color)
-        # 旧蛇头
-        x1, y1, x2, y2 = snake.bodys[-2]
-        self.cv.create_rectangle(x1, y1, x2, y2, fill=snake.body_color)
-        # 删除一个尾巴
-        x1, y1, x2, y2 = snake.bodys[0]
-        self.cv.create_rectangle(x1, y1, x2, y2, fill="red")
-        # 新食物
-        food = kwargs.get("food")
-        if food:
-            fp = kwargs.get("fp")
-            self.food(fp)
-
-
-# 蛇
 class Snake(object):
     def __init__(self):
         self.bodys = []
-        for i in xrange(0, 3):
-            self.bodys.append((20 * i, 20 * 0, 20 * i + 20, 20 * 0 + 20))
-        self.head_color = "yellow"
-        self.body_color = "black"
+        self.head_color = SNAKE_BG
+        self.body_color = SNAKE_BG
+
+    def del_tail(self):
+        return self.bodys.pop(0)
+
+    def add_head(self, head):
+        self.bodys.append(head)
+
+    def init_body(self, body):
+        self.bodys.append(body)
+
+    def get_head(self):
+        return self.bodys[-1]
 
 
-# 全局蛇
+# 全局单例蛇
 snake = Snake()
-my_direction = 'd'
 
 
-class Controller(threading.Thread):
+# 画面
+class View(object):
+    '''
+    画面渲染
+    '''
+
     def __init__(self):
-        '''
-        游戏初始化
-        '''
-        super(Controller, self).__init__()
+        self.top = Tkinter.Tk()
+        self.top.geometry("400x400")
+        self.top.title("贪吃蛇")
+        self.top.resizable(width=False, height=False)
+        self.cv = Tkinter.Canvas(self.top, bg=MAP_BG, height=VIEW_HIGH, width=VIEW_WIDTH)
+        self.cv.pack(side="left", ipadx=135, ipady=130)
+
+    def move_food(self, points, food):
+        x1, y1, x2, y2 = points
+        self.cv.coords(food, x1, y1, x2, y2)
+
+    def create_food(self, points):
+        x1, y1, x2, y2 = points
+        food = self.cv.create_rectangle(x1, y1, x2, y2, fill=FOOD_BG)
+        return food
+
+    def snake(self):
+        color = snake.body_color
+        for i in xrange(0, 3):
+            if i == 2:
+                color = snake.head_color
+            cv = self.cv.create_rectangle(i * BODY, 0, i * BODY + BODY, BODY, fill=color)
+            snake.init_body(cv)
+
+    def set_head(self, points):
+        x1, y1, x2, y2 = points
+        head = self.cv.create_rectangle(x1, y1, x2, y2, fill=SNAKE_BG)
+        return head
+
+    def get_head_points(self, head):
+        return tuple(self.cv.coords(head))
+
+    def over(self):
+        self.cv.delete("all")
+        b = Tkinter.Button(self.cv, text="继续游戏", width=15)
+        b.pack(side='bottom', pady=20)
+        w = Tkinter.Label(self.cv, text="游戏结束", bg="#030915", fg="white", font=(None, 15))
+        w.pack(side="bottom", pady=140)
+
+
+# 控制器
+class Controller(object):
+    '''
+    游戏逻辑控制
+    '''
+
+    def __init__(self):
         self.direction = Direction.right
         self.fx = 0
         self.fy = 0
-        self._View = View()
+        self.food = -1
+        self.over = False
+        self.view = View()
 
     def move(self):
-        # 旧蛇头
-        x1, y1, x2, y2 = snake.bodys[-1]
+        if self.over:
+            return
+        head = snake.get_head()
+        x1, y1, x2, y2 = self.view.get_head_points(head)
         if self.direction == Direction.up:
-            y1 = y1 - BODY
-            y2 = y2 - BODY
+            y1 -= BODY
+            y2 -= BODY
         elif self.direction == Direction.down:
-            y1 = y1 + BODY
-            y2 = y2 + BODY
+            y1 += BODY
+            y2 += BODY
         elif self.direction == Direction.left:
-            x1 = x1 - BODY
-            x2 = x2 - BODY
+            x1 -= BODY
+            x2 -= BODY
         elif self.direction == Direction.right:
-            x1 = x1 + BODY
-            x2 = x2 + BODY
-        # 新蛇头
-        points = (x1, y1, x2, y2)
-        snake.bodys.append(points)
-        food = False
-        fp = (0,)
+            x1 += BODY
+            x2 += BODY
+        new_head = self.view.set_head((x1, y1, x2, y2))
+        snake.add_head(new_head)
+        # 被吃后重新移动食物
         if x1 == self.fx and y1 == self.fy:
-            food = True
-            fp = self.foot()
-            self.fx, self.fy, _, _ = fp
+            fp = self.set_foot()
+            self.view.move_food(fp, self.food)
+        else:
+            tail = snake.del_tail()
+            self.view.cv.delete(tail)
         # 碰撞检测
+        points = (x1, y1, x2, y2)
         border, body = self.checking(points)
-        over = True if border or body else False
-        self._View.update(food=food, fp=fp, over=over)
-        if not food:
-            snake.bodys.pop(0)
-        self._View.cv.after(1000, self.move)
+        if border or body:
+            self.over = True
+            self.view.over()
+        self.view.cv.after(SPEED, self.move)
 
     def checking(self, points):
         '''
@@ -141,18 +151,13 @@ class Controller(threading.Thread):
             border = True
         return border, body
 
-    def over(self):
-        pass
-
     def start_game(self):
-        # 初始化地图
-        self._View.draw()
-        self.fx, self.fy, x2, y2 = self.foot()
-        self._View.food((self.fx, self.fy, x2, y2))
-        self._View.snake()
+        x1, y1, x2, y2 = self.set_foot()
+        self.food = self.view.create_food((x1, y1, x2, y2))
+        self.view.snake()
         self.move()
-        self._View.top.bind('<Key>', self.key_pressed)
-        self._View.cv.mainloop()
+        self.view.top.bind('<Key>', self.key_pressed)
+        self.view.top.mainloop()
 
     def key_pressed(self, e):
         direction = e.char
@@ -169,13 +174,22 @@ class Controller(threading.Thread):
             if self.direction != Direction.left:
                 self.direction = Direction.right
 
-    def foot(self):
+    def set_foot(self):
         # 绘食物
-        x = random.randint(0, VIEW_WIDTH / BODY)
-        y = random.randint(0, VIEW_HIGH / BODY)
-        # 遍历一遍蛇身保证食物不能出现在蛇身上
+        r = VIEW_WIDTH / BODY
+        x = random.randint(0, r - 1)
+        y = random.randint(0, r - 1)
+        if x == r and y == r:
+            self.over = True
+        self.fx = BODY * x
+        self.fy = BODY * y
         return tuple([BODY * x, BODY * y, BODY * x + BODY, BODY * y + BODY])
 
 
+def Game():
+    con = Controller()
+    con.start_game()
+
+
 if __name__ == '__main__':
-    app()
+    Game()
